45c236f35fbd54af777f96db39ac344fc60f4d49
comphsh <comphsh@gmail.com>
2024-08-08 17:40:59 +0800

    可能是第六回，增加随机种子 val = torch.randint(1000 , (2,4))

--- first_.py
+++ first_.py
@@ -4,3 +4,12 @@ print("good")
 
 for i in range(12):
     print(i)
+
+val = torch.randint(1000 , (2,4))
+val = val[0][0]
+print(val)
+if val > 13:
+    print("this is larger than 13")
+else:
+    print("this is smaller than 13")
+
