67b5959ba23c1b08279290816e4e52fe8b7b306a
comphsh <comphsh@gmail.com>
2024-08-08 15:47:17 +0800

    第四回写注释，continuing

--- first_.py
+++ first_.py
@@ -4,3 +4,19 @@ print("good")
 
 for i in range(12):
     print(i)
+
+
+A = torch.tensor([1,2])
+B = torch.tensor([5,8])
+C = A+B
+print(C)
+
+print("3nd write annotation")
+
+
+
+
+
+print("3nd ending")
+
+print("3 3nd continuing")
